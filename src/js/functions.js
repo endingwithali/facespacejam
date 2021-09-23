d18f0d811ef0a324b0cc0d6c168dbbf09e20199c
Ali <ali@finkelstein.us>
2021-09-23 13:21:11 -0400

    updating netlify information

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,56 +1,34 @@
 async function submit(){
-    var file = document.getElementById("fileUpload").files[0]
-    const reader = new FileReader();
-    if (file) {
-        reader.onerror = function(e){
-            console.log(e)
-        }  
-        reader.onloadend = async function (e) {
-            var jpg = true;
-            var image = null;
-            try {
-              image = atob(e.target.result.split("data:image/jpeg;base64,")[1]);
-    
-            } catch (e) {
-              jpg = false;
+  var file = document.getElementById("fileUpload").files[0]
+  var reader = new FileReader();
+  if (file) {
+      reader.onerror = function(e){
+          console.log(e)
+      }  
+      reader.onloadend = async function (e) {
+          await fetch(`/.netlify/functions/aws-rekog`, {
+              method: 'POST',
+              body: JSON.stringify({
+                    image: e.target.result,
+                  }),
+              headers: { 'Content-Type': 'application/json' },
+          }).then(response =>{
+            if (response.status != '200'){
+              // throw (errror)
+              return; 
             }
-            if (jpg == false) {
-              try {
-                image = atob(e.target.result.split("data:image/png;base64,")[1]);
-              } catch (e) {
-                alert("Not an image file Rekognition can process");
-                return;
-              }
+            console.log(response.status)
+            return response.json()
+          }).then(spicyResponse=>{
+            if (spicyResponse){
+              console.log(spicyResponse)
             }
-            // console.log(e.target.result)
-            console.log(image)
-            await modifyImage(image)
-            // process image to aws lambda function
+            return;
+            
+          })
 
-        }
-        const imageData = reader.readAsDataURL(file);
-        console.log(imageData)
-        // await modifyImage(imageData)
+      }
+      const imageData = reader.readAsDataURL(file);
+
     }
-    
-}
-
-async function modifyImage(imageData){
-    // console.log(imageData)
-    // console.log("in modify")
-    //${process.env.NETLIFY_URL}/.netlify/functions/aws-rekog
-    await fetch(`/.netlify/functions/aws-rekog`, {
-        method: 'POST',
-        body: JSON.stringify({
-            image: imageData,
-        }),
-        headers: { 'Content-Type': 'application/json' },
-    })
-    .then((res) => {
-        console.log(res.json())
-        console.log("return")
-        return res
-    })
-    // .then((response) => {
-    // });
-}
+  }
